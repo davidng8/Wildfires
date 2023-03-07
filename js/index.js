35a468f92ba6e302bbe0b1ad44ff4f7ca25d9b34
Frost Song <songy54@uw.edu>
2023-03-06 19:57:03 -0800

    Fix map toggle

--- js/index.js
+++ js/index.js
@@ -30,9 +30,6 @@ map = new mapboxgl.Map({
 let large_fires = {
   'id': 'lg-fire-polies',
   'type': 'fill',
-  'layout': {
-    'visibility': 'none'
-},
   'source': 'large-fires',
   'layout': {
     // Make the layer visible by default.
@@ -48,9 +45,6 @@ let large_fires = {
 let pre_07 = {
   'id': 'fires-pre-07',
   'type': 'circle',
-  'layout': {
-    'visibility': 'none'
-},
   'source': 'dnr-90-07',
   'layout': {
     'visibility': 'none'
@@ -100,7 +94,7 @@ map.on('load', () => {
   map.addLayer(pre_07);
 });
 
-//------------------------------------------------------------------------------
+//-------------------------------Map Toggles------------------------------------
 // Map 1 toggle on/off
 id("check1").addEventListener("change", (e) => {
   if (e.target.checked) {
@@ -118,61 +112,6 @@ id("check2").addEventListener("change", (e) => {
     map.setLayoutProperty('fires-pre-07', 'visibility', 'none');
   }
 });
-// This is where I started adding the toggle
-map.on('idle', () => {
-  // If these two layers were not added to the map, abort
-  if (!map.getLayer('large_fires') || !map.getLayer('pre_07')){
-      return;
-  }
-
-  // Enumerate ids of the layers.
-  const toggleableLayerIds = ['lg-fire-polies', 'fires-pre-07'];
-
-  // Set up the corresponding toggle button for each layer.
-  for (const id of toggleableLayerIds) {
-      // Skip layers that already have a button set up.
-      if (document.getElementById(id)) {
-          continue;
-      }
-
-      // Create a link.
-      const link = document.createElement('a');
-      link.id = id;
-      link.href = '#';
-      link.textContent = id;
-      link.className = 'inactive';
-
-      // Show or hide layer when the toggle is clicked.
-      link.onclick = function (e) {
-          const clickedLayer = this.textContent;
-          // preventDefault() tells the user agent that if the event does not get explicitly handled, 
-          // its default action should not be taken as it normally would be.
-          e.preventDefault();
-          // The stopPropagation() method prevents further propagation of the current event in the capturing 
-          // and bubbling phases. It does not, however, prevent any default behaviors from occurring; 
-          // for instance, clicks on links are still processed. If you want to stop those behaviors, 
-          // see the preventDefault() method.
-          e.stopPropagation();
-
-          const visibility = map.getLayoutProperty(
-              clickedLayer,
-              'visibility'
-          );
-
-          // Toggle layer visibility by changing the layout object's visibility property.
-          // if it is currently visible, after the clicking, it will be turned off.
-          if (visibility === 'visible') {
-              map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-              this.className = '';
-          } else { //otherise, it will be turned on.
-              this.className = 'active';
-              map.setLayoutProperty(
-                  clickedLayer,
-                  'visibility',
-                  'visible'
-              );
-          }
-      };
 
 /*------------------------------Helper functions------------------------------*/
 // Get id shortcut.
